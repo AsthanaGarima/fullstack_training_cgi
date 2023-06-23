f75388c7dd4e38f276a1db98e842d6cd3fef149a
AsthanaGarima <132817124+AsthanaGarima@users.noreply.github.com>
2023-06-23 23:25:53 +0530

    Add files via upload

---
+++ order-service/src/main/java/com/example/demo/OrderServiceApplication.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Order;
+import com.example.demo.repo.OrderRepository;
+
+import lombok.AllArgsConstructor;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@AllArgsConstructor
+@RestController
+public class OrderServiceApplication  {
+
+	private OrderRepository orderRepository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+	@GetMapping
+	public ResponseEntity<?> sayHello(@RequestParam("name") String name)
+	{
+		return ResponseEntity.ok("hello" +name);
+	}
+
+	
+}
